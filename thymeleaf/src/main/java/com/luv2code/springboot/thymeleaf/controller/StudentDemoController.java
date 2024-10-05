9110777d1c4d7c93c51d6fff9e667751293cc393
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2024-10-05 15:16:54 +0300

    for White Space Validation (InitBinder)

--- thymeleaf/src/main/java/com/luv2code/springboot/thymeleaf/controller/StudentDemoController.java
+++ thymeleaf/src/main/java/com/luv2code/springboot/thymeleaf/controller/StudentDemoController.java
@@ -1,10 +1,15 @@
 package com.luv2code.springboot.thymeleaf.controller;
 
 import com.luv2code.springboot.thymeleaf.entity.Student;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -22,6 +27,12 @@ public class StudentDemoController {
     @Value("${Systems}")
     List<String> systems;
 
+    @InitBinder
+    public void initBinder(WebDataBinder theDataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        theDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
     @GetMapping("/showStudentForm")
     public String showStudentForm(Model model) {
         model.addAttribute("student", new Student());
@@ -32,7 +43,10 @@ public class StudentDemoController {
     }
 
     @PostMapping("/processStudentForm")
-    public String confirmStudentForm(@ModelAttribute("student") Student student) {
-        return "confirmStudentForm";
+    public String confirmStudentForm(@Valid @ModelAttribute("student") Student student, BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "studentForm";
+        else
+            return "confirmStudentForm";
     }
 }
--- thymeleaf/src/main/java/com/luv2code/springboot/thymeleaf/entity/Student.java
+++ thymeleaf/src/main/java/com/luv2code/springboot/thymeleaf/entity/Student.java
@@ -1,5 +1,8 @@
 package com.luv2code.springboot.thymeleaf.entity;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +15,11 @@ import lombok.Setter;
 public class Student {
 
     private String firstName;
+
+    @NotNull(message = "is Required!")
+    @Size(min = 1 ,message = "must more than 1 character")
     private String lastName;
+
     private String country;
     private String programmingLanguage;
     private String system;
