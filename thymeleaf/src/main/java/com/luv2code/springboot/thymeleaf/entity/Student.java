694477c804f49cc5adfeeb536c2d93531bc6f0b8
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2024-10-05 18:01:32 +0300

    for Regex  Validation (Pattern)

--- thymeleaf/src/main/java/com/luv2code/springboot/thymeleaf/entity/Student.java
+++ thymeleaf/src/main/java/com/luv2code/springboot/thymeleaf/entity/Student.java
@@ -1,9 +1,6 @@
 package com.luv2code.springboot.thymeleaf.entity;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,9 +18,13 @@ public class Student {
     @NotNull(message = "is Required!")
     @Size(min = 1 ,message = "must more than 1 character")
     private String lastName;
+
     @Min(value = 12 ,message = "must greater than 12")
     @Max(value = 33 , message = "must less than 33")
     private int age;
+
+    @Pattern(regexp = "^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\\d{4})$",message = "Valid input , must in form (dd-mm-yyyy)")
+    private String birthDate;
 
     private String country;
     private String programmingLanguage;
