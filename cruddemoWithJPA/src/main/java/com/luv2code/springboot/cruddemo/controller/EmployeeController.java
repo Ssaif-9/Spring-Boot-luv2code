203a8bdff57daca51bef3665e852cba21e1b2e44
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2024-10-07 15:47:10 +0300

    Get Employee Done !..

---
+++ cruddemoWithJPA/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package com.luv2code.springboot.cruddemo.controller;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeServiceInterface;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeServiceInterface employeeServiceInterface;
+
+    public EmployeeController(EmployeeServiceInterface employeeServiceInterface) {
+        this.employeeServiceInterface = employeeServiceInterface;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model){
+        List<Employee> listEmployee =employeeServiceInterface.getAllEmployee();
+        model.addAttribute("employees", listEmployee);
+        return "EmployeeList";
+    }
+
+
+}
