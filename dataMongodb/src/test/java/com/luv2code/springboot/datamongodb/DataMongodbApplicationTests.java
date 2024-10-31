0a2249b62db799cf5cf1d448d6ce8b83a5eade20
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2024-10-31 17:14:30 +0300

    Tyt Mongo Database..!

---
+++ dataMongodb/src/test/java/com/luv2code/springboot/datamongodb/DataMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.datamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
