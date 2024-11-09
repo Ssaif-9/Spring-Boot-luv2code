4b7abe89f5ac5a4b845614423848d421525d38b7
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2024-11-09 20:27:51 +0200

    Section 9 : Relational Database

--- cruddemowithrelationaldatabase/src/main/java/com/luv2code/springboot/cruddemowithrelationaldatabase/reposatity/InstructorRepoInterface.java
+++ cruddemowithrelationaldatabase/src/main/java/com/luv2code/springboot/cruddemowithrelationaldatabase/reposatity/InstructorRepoInterface.java
@@ -2,9 +2,12 @@ package com.luv2code.springboot.cruddemowithrelationaldatabase.reposatity;
 
 import com.luv2code.springboot.cruddemowithrelationaldatabase.entity.Instructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InstructorRepoInterface extends JpaRepository<Instructor, Integer> {
+
+
 
 }
