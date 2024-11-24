fdd97576dafad9bc38e893fd7059a87e94b72e12
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2024-11-24 14:01:08 +0200

    Section 9 : Relational Database

--- Banking/src/main/java/com/luv2code/springboot/banking/reposatiry/TransactionRepo.java
+++
@@ -1,9 +0,0 @@
-package com.luv2code.springboot.banking.reposatiry;
-
-import com.luv2code.springboot.banking.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepo extends JpaRepository<Transaction, Long> {
-}
